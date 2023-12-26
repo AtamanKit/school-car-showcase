a190bceb81b95e3648acabf73ea08f5e6c201b41
AtamanKit <rodionatamaniuc@gmail.com>
2023-12-26 18:23:26 +0200

    CarCard.tsx, CarCetails.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -12,9 +12,24 @@ const SearchManufacturer = ({
 }: SearchManufacturerProps) => {
     const [query, setQuery] = useState('');
 
+// const SearchManufacturer = () => {
+//     const [query, setQuery] = useState('');
+
+    const filteredManufacturers = 
+        query === ""
+        ? manufacturers
+        : manufacturers.filter((item) => (
+            item.toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, "")
+        )))
+    
     return (
         <div className="search-manufacturer">
-            <Combobox>
+            <Combobox
+                value={manufacturer}
+                onChange={setManufacturer}
+            >
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image
@@ -39,7 +54,34 @@ const SearchManufacturer = ({
                         afterLeave={() => setQuery('')}
                     >
                         <Combobox.Options>
-
+                            {filteredManufacturers.map((item) => (
+                                    <Combobox.Option
+                                        key={item}
+                                        className={({ active }) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                                        value={item}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                            <span
+                                                className={`block truncate ${
+                                                selected ? 'font-medium' : 'font-normal'
+                                                }`}
+                                            >
+                                                {item}
+                                            </span>
+                                            {selected ? (
+                                                <span
+                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                    active ? 'text-white' : 'text-teal-600'
+                                                }`}
+                                                >
+                                                </span>
+                                            ) : null}
+                                            </>
+                                        )}
+                                    </Combobox.Option>
+                                )
+                            )}
                         </Combobox.Options>
 
                     </Transition>
